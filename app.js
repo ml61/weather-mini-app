97c3acf485e7d233a50e2826f654519eb4535202
ml61 <mlysenko0601@gmail.com>
2021-01-30 18:14:32 +0100

    final version of async await option

--- app.js
+++ app.js
@@ -1,50 +1,51 @@
-window.addEventListener("load", () => {
+let locationElement = document.querySelector(".location");
+let locationTimezone = document.querySelector(".location-timezone");
+let icon = document.querySelector("img");
+let temperatureElement = document.querySelector(".temperature");
+let temperatureDescription = document.querySelector(".temperature-description");
+let temperatureDegree = document.querySelector(".temperature-degree");
+
+window.addEventListener("load", getCoords);
+
+async function getCoords() {
   let long;
   let lat;
-  let temperatureDescription = document.querySelector(
-    ".temperature-description"
-  );
-  let temperatureDegree = document.querySelector(".temperature-degree");
-  let locationTimezone = document.querySelector(".location-timezone");
-  let icon = document.querySelector("img");
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
-      const api = `http://api.weatherstack.com/current?access_key=1834df75898853aa90a398e38d879f26&query=${lat},${long}`;
-      fetch(api)
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          console.log(data);
-          const {
-            temperature,
-            weather_descriptions: weatherDescriptions,
-            weather_icons: weatherIcons,
-          } = data.current;
-          const { country, name } = data.location;
-          //set dom elements from the API
-          temperatureDegree.textContent = temperature;
-          temperatureDescription.textContent = weatherDescriptions[0];
-          locationTimezone.textContent = `${country}, ${name}`;
-          icon.src = weatherIcons[0];
-        });
+      requestWeather(lat, long);
     });
   } else {
     h1.textContent = "Make able geolocation";
   }
-});
+}
 
-// async function getWeather(lat, long) {
-//   try {
-//     let respond = await fetch(
-//       `http://api.weatherstack.com/current?access_key=1834df75898853aa90a398e38d879f26&query=${lat},${long}`
-//     );
-//     let objWeather = await respond.json();
-//     return objWeather;
-//   } catch (err) {
-//     console.error(err);
-//   }
-// }
+async function requestWeather(lat, long) {
+  const api = `http://api.weatherstack.com/current?access_key=1834df75898853aa90a398e38d879f26&query=${lat},${long}`;
+  let response = await fetch(api);
+  console.log(response);
+  let data = await response.json();
+  console.log(data);
+  const {
+    temperature,
+    weather_descriptions: weatherDescriptions,
+    weather_icons: weatherIcons,
+  } = data.current;
+  const { country, name } = data.location;
+  changeLocation(country, name, weatherIcons);
+  changeTemperature(temperature, weatherDescriptions);
+}
+
+function changeLocation(country, name, weatherIcons) {
+  locationTimezone.textContent = `${country}, ${name}`;
+  icon.src = weatherIcons[0];
+  locationElement.classList.remove("hidden");
+}
+
+function changeTemperature(temperature, weatherDescriptions) {
+  temperatureDegree.textContent = temperature;
+  temperatureDescription.textContent = weatherDescriptions[0];
+  temperatureElement.classList.remove("hidden");
+}
